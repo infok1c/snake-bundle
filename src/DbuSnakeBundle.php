d7ac8c54877e023dab6adc018c958ec9b9e72ef6
infok1c <quentin.cornet.k1c@free.fr>
2026-03-26 22:48:21 +0100

    Migrated DbuSnakeBundle from Bundle to AbstractBundle

--- src/DbuSnakeBundle.php
+++ src/DbuSnakeBundle.php
@@ -2,8 +2,18 @@
 
 namespace Dbu\SnakeBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Dbu\SnakeBundle\Command\SnakeCommand;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-class DbuSnakeBundle extends Bundle
+class DbuSnakeBundle extends AbstractBundle
 {
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->services()
+            ->set(SnakeCommand::class)
+            ->autoconfigure()
+            ->autowire();
+    }
 }
